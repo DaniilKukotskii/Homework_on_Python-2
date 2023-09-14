b476be6fac9ceb6a4e3ed51af9991bca77bbd56d
Daniil <daniilkukotsky@gmail.com>
2023-09-14 23:27:54 +0400

    First and Final commit third  task

---
+++ Task3.py
@@ -0,0 +1,28 @@
+# Требуется вывести все целые степени двойки (т.е. числа вида 2k), не превосходящие числа N.
+
+def user_enter():
+    while True:
+        try:
+            user_num = int(input("Enter the finite number: "))
+            break
+        except ValueError:
+            print("Invalid input. Please enter an integer.")
+
+    return user_num
+
+
+
+def int_power_of_two(finite_number: int):
+    i = 0
+    while i < finite_number:
+        result = 2**i
+        if result > 10: break
+        i +=1
+        print(result)
+
+
+user_ent = user_enter()
+power_two = int_power_of_two(user_ent)
+
+
+# ADD TRY - EXCEPT!!!
